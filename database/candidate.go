769681bd63950a6491e8ba06839a62e6c8386bec
OGFris <ilyesou.cherfaoui.18@gmail.com>
2019-07-11 20:49:35 +0200

    Created candidates body functions.

--- database/candidate.go
+++ database/candidate.go
@@ -6,9 +6,33 @@
 
 package database
 
+import (
+	"github.com/OGFris/VoteBackend/utils"
+	"github.com/jinzhu/gorm"
+)
+
 type Candidate struct {
 	Model
 	Name        string `gorm:"Type:varchar(255);Column:name;unique;NOT NULL" json:"name"`
 	Party       string `gorm:"Type:varchar(255);Column:party;NOT NULL" json:"party"`
 	Description string `gorm:"Type:mediumtext;Column:description;NOT NULL" json:"description"`
+}
+
+func Exist(name string) bool {
+	return !gorm.IsRecordNotFoundError(Instance.Find(&Candidate{}, &Candidate{Name: name}).Error)
+}
+
+func Create(name, party, description string) error {
+
+	return Instance.Create(&Candidate{
+		Name:        name,
+		Party:       party,
+		Description: description,
+	}).Error
+}
+
+func AllCandidates() (c []Candidate) {
+	utils.PanicError(Instance.Find(&c).Error)
+
+	return
 }
